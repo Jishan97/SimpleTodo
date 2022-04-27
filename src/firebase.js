09c016ba252840ec3d912e182b1ed03c135e8062
jishan shaikh <shaikh.jishan1997@gmail.com>
2022-04-27 02:20:13 +0100

    Added firebase details without hiding

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCioMsv1bVjKVQ_cPL-AkztUSoebjQvE5k",
   authDomain: "todoapp-786b1.firebaseapp.com",
   projectId: "todoapp-786b1",
-  storageBucket:"todoapp-786b1.appspot.com",
+  storageBucket: "todoapp-786b1.appspot.com",
   messagingSenderId: "477673384899",
   appId: "1:477673384899:web:1fb3f238e10e585b6deb76",
   measurementId: "G-ZNKVMKR9TD"
